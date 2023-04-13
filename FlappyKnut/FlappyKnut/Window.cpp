50d63bc8d9cf08439a608e90565a1ff1c3c013eb
Marc Zaku <mzaku@hotmail.de>
2023-04-13 21:22:36 +0200

    Improve DLL Reference

--- FlappyKnut/FlappyKnut/Window.cpp
+++ FlappyKnut/FlappyKnut/Window.cpp
@@ -14,12 +14,15 @@ Window::Window(int width, int height, IImageLoader* imageLoader)
 	}
 	
 	// Create Window and Renderer
-	SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_SHOWN, &window, &renderer);
+	SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE, &window, &renderer);
 	if (!window)
 	{
 		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		return;
 	}
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
+	SDL_RenderSetLogicalSize(renderer, width, height);
 
 	success = true;
 }
@@ -49,7 +52,7 @@ std::unique_ptr<Image> Window::loadImage(const char* path) {
 }
 
 void Window::clear() {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 120, 60, 255, 255);
 	SDL_RenderClear(renderer);
 }
 
